817e14683cf61bbbf4b77b8e08bde51f5c972d40
KARTIK SONI <sonikartik98@outlook.com>
2018-11-11 02:45:47 +0530

    All Nodes Playing Properly

--- Xylophone/ VIewController.swift
+++ Xylophone/ VIewController.swift
@@ -20,7 +20,33 @@ class ViewController: UIViewController{
 
     @IBAction func notePressed(_ sender: UIButton) {
         
-        guard let url = Bundle.main.url(forResource: "note1", withExtension: "wav") else { return }
+        var url : URL = Bundle.main.url(forResource: "note1", withExtension: "wav")!
+        
+        switch sender.tag {
+        case 1:
+            url = Bundle.main.url(forResource: "note1", withExtension: "wav")!
+            break
+        case 2:
+            url = Bundle.main.url(forResource: "note2", withExtension: "wav")!
+            break
+        case 3:
+            url = Bundle.main.url(forResource: "note3", withExtension: "wav")!
+            break
+        case 4:
+            url = Bundle.main.url(forResource: "note4", withExtension: "wav")!
+            break
+        case 5:
+            url = Bundle.main.url(forResource: "note5", withExtension: "wav")!
+            break
+        case 6:
+            url = Bundle.main.url(forResource: "note6", withExtension: "wav")!
+            break
+        case 7:
+            url = Bundle.main.url(forResource: "note7", withExtension: "wav")!
+            break
+        default:
+            print("Wrong Node")
+        }
         
         do {
             try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
